38f3925cd33a21741c144d112a6579a9a8152bfe
Radosław Kintzi <r.kintzi@pobox.net.pl>
2017-11-09 20:36:36 +0100

    Formatting

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.css'],
 })
-export class PreviewComponent implements OnInit {
+export class PreviewComponent implements OnInit, OnChanges {
+
+  private static re = new RegExp(/\*([^\*]*)\*/);
 
   @Input() text: string;
+  formatted: string;
 
   constructor() { }
 
+  private static format(text: string): string {
+      return text.replace(PreviewComponent.re, '<em>$1</em>');
+  }
+
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+      if (changes['text']) {
+          this.formatted = PreviewComponent.format(changes['text'].currentValue);
+      }
   }
 
 }
